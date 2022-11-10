b6a9151a74cbc048f95170f4b62bf91c52635c40
huang0811 <s1100393@gm.pu.edu.tw>
2022-11-10 22:36:36 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,37 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+from flask import Flask, render_template, request
+from datetime import datetime, timezone, timedelta
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>黃鈺慈的vercel</h1>"
+    homepage += "<a href=/read>讀取Firestore資料</a><br>"
+    return homepage
+
+@app.route("/read", methods=["GET", "POST"])
+def read():
+    if request.method == "POST":
+        cond = request.form["keyword"]
+        result = "請輸入您要查詢的課程關鍵字："+ cond  
+
+        db = firestore.client()   
+        collection_ref = db.collection("111")    
+        docs = collection_ref.get()
+        result = ""    
+        for doc in docs:
+            dict = doc.to_dict()
+            if cond in dict["Course"]:         
+                result += dict["Leacture"]+"老師開的"+dict["Course"]+"課程，每周"+dict["Time"]+"於"+dict["Room"]+"上課<br>"  
+            return result
+    else:
+        return render_template("read.html")
+
+#if __name__ == "__main__":
+#    app.run()
