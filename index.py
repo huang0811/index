b75b7ab9960ca628f2595f2b41e2114620d6c6a8
huang0811 <s1100393@gm.pu.edu.tw>
2022-11-11 21:05:51 +0800

    make it better

--- index.py
+++ index.py
@@ -19,18 +19,15 @@ def index():
 def read():
     if request.method == "POST":
         cond = request.form["keyword"]
-        result = "請輸入您要查詢的課程關鍵字："+ cond  
-        #result = "請輸入您要查詢的教師關鍵字："+ tea
+        result = "請輸入您要查詢的課程關鍵字："+ cond
         db = firestore.client()   
         collection_ref = db.collection("111")    
         docs = collection_ref.get()
         result = ""    
         for doc in docs:
             dict = doc.to_dict()
-            if cond in dict["Course"]: #and dict["Leacture"]:         
+            if cond in dict["Course"]:       
                 result += dict["Leacture"]+"老師開的"+dict["Course"]+"課程，每周"+dict["Time"]+"於"+dict["Room"]+"上課<br>"  
-            #if result == "":
-                #result = "Sorry..."
         return result
     else:
         return render_template("read.html")
